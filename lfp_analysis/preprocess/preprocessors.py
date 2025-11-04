7af41e126d937e8cf5cfa9f2121d5da83f72510f
Mohammad <mandyas7613@gmail.com>
2025-11-04 23:16:51 +0330

    add preprocesses such as remove session or select time period of the epochs

--- lfp_analysis/preprocess/preprocessors.py
+++ lfp_analysis/preprocess/preprocessors.py
@@ -16,10 +16,10 @@ class sessionselect(Preprocessor):
     """
     def __init__(self, session_indexs):
         self.session_indexes = session_indexs
-    def process(self, data: np.ndarray, **kwargs: Any) -> np.ndarray:
+    def process(self, signal: np.ndarray, **kwargs: Any) -> np.ndarray:
         """
         """
-        return data[self.session_indexes]
+        return signal[self.session_indexes]
 
 @register("preprocessors","zscore")
 class zscore(Preprocessor):
@@ -32,11 +32,47 @@ class zscore(Preprocessor):
     def __init__(self, dim):
         self.dim = dim
         
-    def process(self, data: np.ndarray, **kwargs: Any) -> np.ndarray:
+    def process(self, signal: np.ndarray, **kwargs: Any) -> np.ndarray:
         """
         """
-        mean = data.mean(axis=self.dim, keepdims=True)
-        std = data.std(axis=self.dim, keepdims=True)
+        mean = signal.mean(axis=self.dim, keepdims=True)
+        std = signal.std(axis=self.dim, keepdims=True)
 
-        zscored = (data - mean) / std
+        zscored = (signal - mean) / std
         return zscored
+
+
+@register("preprocessors","selecttime")
+class timeselect(Preprocessor):
+    """
+    selection sessions
+    independently.
+    Input: numpy array with shape (Number of sessions,...)
+    Output: numpy array with shape(Numbr of selected sessions,....)
+    """
+    def __init__(self, start_index, end_index):
+        self.start_index = start_index
+        self.end_index = end_index
+    def process(self, signal: np.ndarray, **kwargs: Any) -> np.ndarray:
+        """
+        """
+        return signal[:,:,:,self.start_index:self.end_index]
+
+
+@register("preprocessors","removesession")
+class sessionselect(Preprocessor):
+    """
+    selection sessions
+    independently.
+    Input: numpy array with shape (Number of sessions,...)
+    Output: numpy array with shape(Numbr of selected sessions,....)
+    """
+    def __init__(self, session_indexs):
+        self.session_indexes = session_indexs
+    def process(self, signal: np.ndarray, **kwargs: Any) -> np.ndarray:
+        """
+        """
+        number_session = signal.shape[0]
+        all_sessions = np.arange(number_session)
+        sessinos_selected = np.setdiff1d(all_sessions,self.session_indexes)
+        return signal[sessinos_selected]
