8e6d49d04157b73eae2fe31dc48db6a563dc70d4
Mohammad <mandyas7613@gmail.com>
2025-11-04 22:50:11 +0330

    add events loader

--- lfp_analysis/preprocess/loader.py
+++ lfp_analysis/preprocess/loader.py
@@ -31,3 +31,32 @@ class MatLoader(BaseLoader):
         ComplexityRestData = sio.loadmat(path)
         ComplexityRestDataArray = ComplexityRestData["complexityDataStandardChs"]["data"][0][0] 
         return ComplexityRestDataArray
+
+
+
+@register("loaders", "mat_events")
+class MatLoader(BaseLoader):
+    """Load LFP dataset stored as .mat"""
+    def __call__(self, path: str):
+        EpochedData = sio.loadmat(path)
+        LFPdata = EpochedData["epoched_data_config"]["data"][0][0]
+        events = self.extract_event_matrix(EpochedData=EpochedData)
+        data = {'signal': LFPdata, 'events': events}
+        return data
+    
+    def extract_event_matrix(self,EpochedData, n_epochs=120):
+        """
+        Extracts trial type matrix of shape (n_sessions, n_epochs)
+        from epoched stimuli data.
+        """
+
+        stimuli_data = EpochedData["epoched_data_config"]['stimuli_data'][0][0]
+
+        n_sessions = len(stimuli_data)
+        event_matrix = np.zeros((n_sessions, n_epochs), dtype=np.int32)
+
+        for i in range(n_sessions):
+            trial_types = stimuli_data[i][0].squeeze()   # shape: (2*n_epochs_total,)
+            event_matrix[i] = trial_types[0,:n_epochs]     # select first 100 trials
+
+        return event_matrix
